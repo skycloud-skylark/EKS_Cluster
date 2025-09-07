37af4f53866d250a919f3987f66cc1aff47f8816
Manoj Kumar <Manoj Kumar@sky.local>
2025-09-08 04:17:13 +0530

    Initial commit - Add Terraform EKS setup and GitHub Actions workflow

---
+++ terraform/eks.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "eks-cluster-demo"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
