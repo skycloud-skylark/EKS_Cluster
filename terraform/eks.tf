46dbe239ef8382b6f440b2cfdd968a0cc84bbea5
Manoj Kumar <Manoj Kumar@sky.local>
2025-09-10 02:14:18 +0530

    Update infrastructure code and automate workflow trigger

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = "eks-cluster-demo"
+  cluster_name    = "Ekscluster-Demo"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
