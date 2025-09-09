63656fbba16521822b0f07a904783597c81c15df
Manoj Kumar <Manoj Kumar@sky.local>
2025-09-10 05:22:54 +0530

    Update Infrastructure config

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = "Eks-VpcDemo"
+  name = "Eksdemo_Vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
